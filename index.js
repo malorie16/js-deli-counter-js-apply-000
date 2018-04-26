0f262d2a6f0551c00321d2a655c0e954a28ab5b9
malorie16 <malorie18@gmail.com>
2018-04-26 18:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    return "The line is currently:" + katzDeliLine.map(name, position) {
+      if (katzDeliLine[0]) {
+        return `${position}. ${name}`
+      }
+      else {
+        return ` ${position}. ${name}`
+      }
+    }
+  }
+}
